a43237db3489febe5e26162db7ecc7660732e5e0
Phan Vũ Tuấn Anh <88922348+MarcoPhanVu@users.noreply.github.com>
2023-05-06 21:16:01 +0700

    fix undefined element by moving the script to the bottom of the page

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
+const slides = document.querySelectorAll(".mySlides");
+
 let slideIndex = 1;
-showSlides(slideIndex);
+
+// slides[slideIndex-1].style.display = "block";
+
+console.log(slideIndex);
+console.log(slides);
+console.log(slides, "MARLK");
+
 
 // Next/previous controls
 function plusSlides(n) {
-showSlides(slideIndex += n);
+	showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-showSlides(slideIndex = n);
+	showSlides(slideIndex = n);
 }
+
 
 function showSlides(n) {
-let i;
-let slides = document.getElementsByClassName("mySlides");
-let dots = document.getElementsByClassName("dot");
+	console.log("show");
+	console.log(slides);
+	console.log("show");
+	let i;
+	let dots = document.getElementsByClassName("dot");
 
-if (n > slides.length) {slideIndex = 1}
-if (n < 1) {slideIndex = slides.length}
-for (i = 0; i < slides.length; i++) {
-	slides[i].style.display = "none";
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+		console.log(slides);
+	}
+
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
 }
-for (i = 0; i < dots.length; i++) {
-	dots[i].className = dots[i].className.replace(" active", "");
-}
-slides[slideIndex-1].style.display = "block";
-dots[slideIndex-1].className += " active";
-}
+
+showSlides(1);
